89a5f60301bc72180a191d48d3f4a2cf714ad122
Tarun Bansal <bansalt766@gmail.com>
2025-06-25 10:24:33 +0530

    day3 added

--- src/Day3_Arrays/Q1.java
+++ src/Day3_Arrays/Q1.java
@@ -1,5 +1,30 @@
 package Day3_Arrays;
-
+import java.util.Scanner;
+import java.util.Arrays;
 public class Q1 {
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        
+	        System.out.print("Enter an integer: ");
+	        int number = scanner.nextInt();
 
-}
+	        int[] multiplicationResult = new int[4]; // for 6 to 9
+
+	        for (int i = 0; i < 4; i++) {
+	            int multiplier = 6 + i;
+	            multiplicationResult[i] = number * multiplier;
+	        }
+
+	        for (int i = 0; i < 4; i++) {
+	            int multiplier = 6 + i;
+	            multiplicationResult[i] = number * multiplier;
+	        }
+
+	        System.out.println("Multiplication results stored in array: " + Arrays.toString(multiplicationResult));
+
+	        scanner.close();
+	    }
+	}
+
+
+
---
+++ src/Day3_Arrays/Q2.java
@@ -0,0 +1,37 @@
+package Day3_Arrays;
+import java.util.Scanner;
+import java.util.Arrays;
+public class Q2 {
+    	    public static void main(String[] args) {
+    	        Scanner scanner = new Scanner(System.in);
+
+    	        System.out.print("Enter number of rows: ");
+    	        int rows = scanner.nextInt();
+
+    	        System.out.print("Enter number of columns: ");
+    	        int columns = scanner.nextInt();
+
+       	        int[][] matrix = new int[rows][columns];
+ 
+    	        System.out.println("Enter the elements of the 2D array:");
+    	        for (int i = 0; i < rows; i++) {
+    	            for (int j = 0; j < columns; j++) {
+    	                System.out.print("Element [" + i + "][" + j + "]: ");
+    	                matrix[i][j] = scanner.nextInt();
+    	            }
+    	        }
+
+    	        int[] array = new int[rows * columns];
+    	        int index = 0;
+    	        for (int i = 0; i < rows; i++) {
+    	            for (int j = 0; j < columns; j++) {
+    	                array[index] = matrix[i][j];
+    	                index++;
+    	            }
+    	        }
+    	        System.out.println("Elements copied to 1D array: " + Arrays.toString(array));
+
+    	        scanner.close();
+    	 }
+  }
+
